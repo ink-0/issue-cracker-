19e566775048edd316f688951f79e0d3f5dd59a3
Raccoon <nobllizdc@gmail.com>
2021-07-09 21:33:12 +0900

    feat: Issue Delete 기능 추가
    - ghojeong/issue-tracker/#183

--- frontend/issue-cracker/src/components/layout/IssueDetail/IssueDetailBox/IssueDetailSidebar/IssueDeleteButton.tsx
+++ frontend/issue-cracker/src/components/layout/IssueDetail/IssueDetailBox/IssueDetailSidebar/IssueDeleteButton.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
-import DeleteIcon from '@material-ui/icons/Delete';
 import Button from '@material-ui/core/Button';
 import TextGroup from '../../../../common/group/TextGroup';
-import { TYPE as T, BUTTON_NAME as BN } from '../../../../../utils/const';
+import {
+  TYPE as T,
+  BUTTON_NAME as BN,
+  URL as U,
+} from '../../../../../utils/const';
 import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import { useRecoilValue } from 'recoil';
+import { issueDetailState } from '../../../../../store/Recoil';
+import { getDelete } from '../../../../../utils/restAPI';
 
 const IssueDeleteButton = (): JSX.Element => {
+  const issueDetailId = useRecoilValue(issueDetailState);
+  const userToken = localStorage.getItem('token');
+
+  const handleClickDeleteButton = () => {
+    const id = issueDetailId.issueId;
+    const url = `${U.ISSUES}/${id}`;
+    getDelete(url, userToken);
+    window.history.back();
+  };
+
   return (
-    <IssueDeleteButtonDiv>
+    <IssueDeleteButtonDiv onClick={handleClickDeleteButton}>
       <IssueDeleteButtoStyle
         startIcon={
           <DeleteOutlineIcon style={{ fontSize: '14px', color: '#FF3B30' }} />
